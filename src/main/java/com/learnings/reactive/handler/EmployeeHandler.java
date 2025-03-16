2d308b65007043017c74e02a60f0fe1e95fe6de0
arunrocker <m.arunkumarmar12@gmail.com>
2025-03-16 22:35:15 +0530

    Spring Reactive Router Handler in Functional Programming

---
+++ src/main/java/com/learnings/reactive/handler/EmployeeHandler.java
@@ -0,0 +1,44 @@
+package com.learnings.reactive.handler;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.learnings.reactive.dao.EmployeeDao;
+import com.learnings.reactive.dto.Employee;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class EmployeeHandler {
+	
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	public Mono<ServerResponse> getEmployees(ServerRequest request){
+		long start = System.currentTimeMillis();
+		List<Employee> list = null;
+		try {
+			list = employeeDao.getEmployees();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("Execution Completed time"+(end-start));
+		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(list,Employee.class);
+	}
+	public Mono<ServerResponse> getEmployeesStream(ServerRequest request) {
+		long start = System.currentTimeMillis();
+		Flux<Employee> list = employeeDao.getEmployeesStream();
+		long end = System.currentTimeMillis();
+		System.out.println("Execution Completed time"+(end-start));
+		return ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(list,Employee.class);
+	}
+}
---
+++ src/main/java/com/learnings/reactive/router/RouterConfig.java
@@ -0,0 +1,24 @@
+package com.learnings.reactive.router;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.learnings.reactive.handler.EmployeeHandler;
+
+@Configuration
+public class RouterConfig {
+
+	@Autowired
+	private EmployeeHandler handler;
+	@Bean
+	public RouterFunction<ServerResponse> router(){
+		return RouterFunctions.route().GET("/router/stream",handler::getEmployeesStream)
+				.GET("/router/list",handler::getEmployees).build();
+	}
+}
